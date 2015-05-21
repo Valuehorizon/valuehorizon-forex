c10f50fcfd724d8622699666305cc937742d6e92
quincya <qalexander@valuehorizon.com>
2015-05-21 16:14:19 -0400

    Added Currency Test

--- forex/tests/models_tests.py
+++ forex/tests/models_tests.py
@@ -1,5 +1,6 @@
 """Tests for the models of the forex app."""
 from django.test import TestCase
+from ..models import Currency
 
 # from . import factories
 
@@ -16,4 +17,10 @@ class DummyModelTestCase(TestCase):
 
 
 class CurrencyModelTests(TestCase):
-    
+    def setUp(self):
+        Currency.objects.create(name="Test Dollar", symbol="TEST")
+        Currency.objects.create(name="Test Dollar2", symbol="TEST2")
+        
+    def test_if_saved(self):
+            test = Currency.objects.get(symbol="TEST")
+            test2 = Currency.objects.get(symbol="TEST2")
